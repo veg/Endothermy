9f1a00d53166cd170268081889ef4ff1fea743fe
agselberg <tul06229@temple.edu>
2024-01-18 11:50:52 -0500

    endothermy update

---
+++ endothermy_011724/run_HPC_Endothermy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+clear
+
+echo ""
+
+# Download HyPhy Standalone analyses
+git clone https://github.com/veg/hyphy-analyses.git
+
+# Set up the pipeline failure expectations.
+#set -euo pipefail
+
+echo "--- Initialized --- "
+
+# Uncomment if you want to generate an analysis DAG file.
+#snakemake --forceall --dag | dot -Tpdf > ENDOTHERMY_DAG.pdf
+
+echo "Creating 'logs' directory"
+mkdir -p logs
+
+echo "Executing HPC Snakemake command"
+
+# Execute the Snakemake command
+snakemake \
+      -s Snakefile \
+      --cluster-config cluster.json \
+      --cluster "qsub -V -l nodes={cluster.nodes}:ppn={cluster.ppn} -q {cluster.name} -l walltime={cluster.walltime} -e logs -o logs" \
+      --jobs 20 all \
+      --rerun-incomplete \
+      --keep-going \
+      --reason \
+      --latency-wait 60 \
+      --use-conda
+
+
+# End Snakemake command
+
+exit 0
+
+# End of file.
