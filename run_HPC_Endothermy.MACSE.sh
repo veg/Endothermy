5e5fe13e45c4f4999c2cfddaac34a2863a0c93b1
agselberg <tul06229@temple.edu>
2024-02-22 12:54:34 -0500

    commit

---
+++ run_HPC_Endothermy.MACSE.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+clear
+
+echo ""
+
+# Download HyPhy Standalone analyses
+git clone https://github.com/veg/hyphy-analyses.git
+
+# Set up the pipeline failure expectations.
+#set -euo pipefail
+
+echo "--- Initialized --- "
+
+# Uncomment if you want to generate an analysis DAG file.
+#snakemake --forceall --dag | dot -Tpdf > ENDOTHERMY_DAG.pdf
+
+echo "Creating $LOGDIR directory"
+LOGDIR="logs_macse"
+mkdir -p $LOGDIR
+
+echo "Executing HPC Snakemake command"
+
+# Execute the Snakemake command
+
+snakemake \
+      -s Snakefile_MACSE \
+      --cluster-config cluster.json \
+      --cluster "qsub -V -l nodes={cluster.nodes}:ppn={cluster.ppn} -q {cluster.name} -l walltime={cluster.walltime} -e $LOGDIR -o $LOGDIR" \
+      --jobs 100 all \
+      --rerun-incomplete \
+      --keep-going \
+      --reason \
+      --rerun-incomplete \
+      --latency-wait 300 
+
+
+# End Snakemake command
+
+exit 0
+
+# End of file.
